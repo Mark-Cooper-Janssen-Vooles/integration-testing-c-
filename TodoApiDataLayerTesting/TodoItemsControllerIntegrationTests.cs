5b189e5d2ed636e4c5560c33af968e7047f344a8
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2024-02-28 18:13:51 +1100

    Write more integration tests

--- TodoApiDataLayerTesting/TodoItemsControllerIntegrationTests.cs
+++ TodoApiDataLayerTesting/TodoItemsControllerIntegrationTests.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Text;
+using System.Text.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,8 +28,8 @@ public class TodoItemsControllerIntegrationTests:IClassFixture<WebApplicationFac
         var client = _factory.CreateClient();
         dbContext.TodoItems.RemoveRange(dbContext.TodoItems);
         await dbContext.SaveChangesAsync();
-        dbContext.TodoItems.Add(new TodoItem("Test Todo 1") { Name = "Test Todo 1" });
-        dbContext.TodoItems.Add(new TodoItem("Test Todo 2") { Name = "Test Todo 2" });
+        dbContext.TodoItems.Add(new TodoItem("Test Todo 1"));
+        dbContext.TodoItems.Add(new TodoItem("Test Todo 2"));
         await dbContext.SaveChangesAsync();
         
         // act 
@@ -41,5 +43,59 @@ public class TodoItemsControllerIntegrationTests:IClassFixture<WebApplicationFac
         todoItems.Count.Should().Be(2);
         todoItems.Should().Contain(ti => ti.Name == "Test Todo 1" && ti.IsComplete == false);
         todoItems.Should().Contain(ti => ti.Name == "Test Todo 2" && ti.IsComplete == false);
+    }
+
+    [Fact]
+    public async Task GetTodoItemWithId_ReturnsTodoItem()
+    {
+        // arrange 
+        using var scope = _factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<TodoContext>();
+        var client = _factory.CreateClient();
+        var todoItem = new TodoItem("Test Todo 1");
+        dbContext.TodoItems.Add(todoItem);
+        await dbContext.SaveChangesAsync();
+        
+        // act 
+        var response = await client.GetAsync($"/api/TodoItems/{todoItem.Id}");
+        
+        // assert 
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetTodoItemWithId_ReturnsNotFound()
+    {
+        // arrange 
+        using var scope = _factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<TodoContext>();
+        var client = _factory.CreateClient();
+        dbContext.TodoItems.RemoveRange(dbContext.TodoItems);
+        await dbContext.SaveChangesAsync();
+        
+        // act 
+        var response = await client.GetAsync("/api/TodoItems/1");
+        
+        // assert 
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task PostTodoITemAsync_ShouldCreateNEwTodoItem()
+    {
+        //Arrange
+        var client = _factory.CreateClient();
+        var todoItem = new TodoItem("New todo item");
+        var jsonContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(todoItem), Encoding.UTF8, "application/json");
+        // act 
+        var response = await client.PostAsync("/api/TodoItems", jsonContent);
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var createdTodoItem = System.Text.Json.JsonSerializer.Deserialize<TodoItem>(responseContent,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        createdTodoItem.Should().NotBeNull();
+        createdTodoItem.Name.Should().Be(todoItem.Name);
+        createdTodoItem.IsComplete.Should().Be(false);
     }
 }
