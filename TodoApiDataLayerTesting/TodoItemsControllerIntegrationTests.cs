043e43402d91cb5ad4d590cd4dd36e7e5377cf7a
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2024-02-28 18:27:57 +1100

    Add Put api integration test

--- TodoApiDataLayerTesting/TodoItemsControllerIntegrationTests.cs
+++ TodoApiDataLayerTesting/TodoItemsControllerIntegrationTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using FluentAssertions;
@@ -92,6 +93,36 @@ public class TodoItemsControllerIntegrationTests:IClassFixture<WebApplicationFac
         // assert
         response.StatusCode.Should().Be(HttpStatusCode.Created);
         var responseContent = await response.Content.ReadAsStringAsync();
+        var createdTodoItem = System.Text.Json.JsonSerializer.Deserialize<TodoItem>(responseContent,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        createdTodoItem.Should().NotBeNull();
+        createdTodoItem.Name.Should().Be(todoItem.Name);
+        createdTodoItem.IsComplete.Should().Be(false);
+    }
+
+    [Fact]
+    public async Task PutTodoItemAsync_ShouldUpdateTodoItem()
+    {
+        // arrange 
+        using var scope = _factory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<TodoContext>();
+        var client = _factory.CreateClient();
+        var todoItem = new TodoItem("Test Todo 1");
+        dbContext.TodoItems.Add(todoItem);
+        await dbContext.SaveChangesAsync();
+
+        var updatedTodoItem = new TodoItem("Updated Todo");
+        updatedTodoItem.Id = todoItem.Id;
+        
+        var jsonContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(updatedTodoItem), Encoding.UTF8, "application/json");
+        
+        // act 
+        var putResponse = client.PutAsJsonAsync($"/api/TodoItems/{todoItem.Id}", jsonContent);
+        
+        // assert
+        // putResponse.EnsureSuccessStatusCode();
+        var getResponse = await client.GetAsync($"/api/TodoItems/{updatedTodoItem.Id}");
+        var responseContent = await getResponse.Content.ReadAsStringAsync();
         var createdTodoItem = System.Text.Json.JsonSerializer.Deserialize<TodoItem>(responseContent,
             new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         createdTodoItem.Should().NotBeNull();
